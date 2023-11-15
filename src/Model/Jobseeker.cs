eed9554d5d449c709492a248fd201bf4fc0e9e6c
Robin Lundahl <robin.lundahl@hotmail.se>
2023-11-15 14:53:32 +0100

    Jobseeker class created

---
+++ src/Model/Jobseeker.cs
@@ -0,0 +1,10 @@
+public class Jobseeker
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Jobseeker(string name)
+    {
+        Name = name;
+    }
+}
---
+++ src/UI/CreateJobseeker.cs
@@ -0,0 +1,8 @@
+public class CreateJobseeker
+{
+    //Console.WriteLine("Enter your name:");
+    string name = Console.ReadLine();
+
+    Jobseeker j = new Jobseeker(name);
+
+}
