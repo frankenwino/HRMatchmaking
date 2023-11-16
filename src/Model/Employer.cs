673c27521fc8dafc1db889349334986ddcea5757
Robin Lundahl <robin.lundahl@hotmail.se>
2023-11-16 10:13:29 +0100

    Add Employer too Create class

--- src/Model/Employer.cs
+++ src/Model/Employer.cs
@@ -10,23 +10,25 @@ public class Employer
     public string City { get; set; }
     public int CityId { get; set; }
 
-    public Employer(string name, string organisationNumber, string industry, int industryId, string email, string city, int cityId)
+    public Employer(string name, string organisationNumber, string industry, int industryId, string email, int cityId)
     {
         Name = name;
         OrganisationNumber = organisationNumber;
         Industry = industry;
         IndustryId = industryId;
         Email = email;
-        City = city;
         CityId = cityId;
     }
 
-    public Employer(string name, string organisationNumber, int industryId, string email, int cityId)
+    public Employer(int id, string name, string organisationNumber, int industryId, string email, int cityId)
     {
+        Id = id;
         Name = name;
         OrganisationNumber = organisationNumber;
         IndustryId = industryId;
         Email = email;
         CityId = cityId;
     }
+
+
 }
