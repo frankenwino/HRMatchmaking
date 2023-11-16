877f82161acdc0b009c65f194c8373f88b8010ab
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2023-11-16 10:06:59 +0100

    Testing changes

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DB.Setup;
 using UI;
+using Model;
 
 Console.Clear();
 // SetUpDB(); // Comment out after setting up DB connecting to the data base
@@ -10,12 +11,11 @@ JobSeekerRepository jsr = new();
 // menu.RunMainMenu();
 
 // Add some jobseekers
-
-for (int i = 0; i < 2; i++)
-{
-    JobSeeker j = Create.JobSeeker();
-    j.Id = jsr.AddJobSeeker(j);
-}
+// for (int i = 0; i < 2; i++)
+// {
+//     JobSeeker j = Create.JobSeeker();
+//     j.Id = jsr.AddJobSeeker(j);
+// }
 
 // JobSeeker j = Create.JobSeeker();
 /* System.Console.WriteLine(j.Id);
@@ -26,35 +26,39 @@ System.Console.WriteLine(j.Email); */
 // j.Id = jsr.AddJobSeeker(j);
 
 // Get all jobseekers and display them
-// IEnumerable<JobSeeker> jobSeekers = jsr.GetAllJobSeekers();
-// System.Console.WriteLine("All Jobseekers in DB");
-// foreach (JobSeeker jobSeeker in jobSeekers)
-// {
-//     System.Console.WriteLine(j.Id);
-//     System.Console.WriteLine(j.Name);
-//     System.Console.WriteLine(j.PersonNumber);
-//     System.Console.WriteLine(j.CityId);
-//     System.Console.WriteLine(j.Telephone);
-//     System.Console.WriteLine(j.Email);
-//     System.Console.WriteLine();
-// }
-// System.Console.WriteLine();
+IEnumerable<JobSeeker> jobSeekers = jsr.GetAllJobSeekers();
+System.Console.WriteLine("All Jobseekers in DB");
+foreach (JobSeeker jobSeeker in jobSeekers)
+{
+    System.Console.WriteLine(jobSeeker.Id);
+    System.Console.WriteLine(jobSeeker.Name);
+    System.Console.WriteLine(jobSeeker.PersonNumber);
+    System.Console.WriteLine(jobSeeker.CityId);
+    System.Console.WriteLine(jobSeeker.Telephone);
+    System.Console.WriteLine(jobSeeker.Email);
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
 
 // Get jobseeker with id 1
-JobSeeker k = jsr.GetJobSeekerById(2);
-System.Console.WriteLine($"Jobseeker with id {k.Id}: {k.Name} {k.PersonNumber}");
-System.Console.WriteLine(k.Id);
-System.Console.WriteLine(k.Name);
-System.Console.WriteLine(k.PersonNumber);
-System.Console.WriteLine(k.CityId);
-System.Console.WriteLine(k.Telephone);
-System.Console.WriteLine(k.Email);
+// JobSeeker k = jsr.GetJobSeekerById(2);
+// System.Console.WriteLine($"Jobseeker with id {k.Id}: {k.Name} {k.PersonNumber}");
+// System.Console.WriteLine(k.Id);
+// System.Console.WriteLine(k.Name);
+// System.Console.WriteLine(k.PersonNumber);
+// System.Console.WriteLine(k.CityId);
+// System.Console.WriteLine(k.Telephone);
+// System.Console.WriteLine(k.Email);
 
 void SetUpDB()
 {
     DBSetup dbs = new();
     dbs.DropAllTables();
     dbs.CreateAllTables();
+
+    PopulateDB popDb = new();
+    popDb.AddJobSeekers();
+
     System.Console.WriteLine("Comment out the line calling SetUpDB() - on line 6 or so.");
     Environment.Exit(0);
 }
