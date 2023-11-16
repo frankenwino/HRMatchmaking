79813bff1b453e64145e3898bda66cea105b4fb9
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2023-11-16 09:52:48 +0100

    Uses Model namespace

--- src/UI/Create.cs
+++ src/UI/Create.cs
@@ -1,22 +1,20 @@
 using ConsoleInputUtility;
+using Model;
 public static class Create
 {
     public static JobSeeker JobSeeker()
     {
-        // string name = ConsoleInput.GetString("Enter name");
-        // DateOnly dateOfBirth = ConsoleInput.GetDateOnly("Enter date of birth [YYYY-MM-DD]");
+        string name = ConsoleInput.GetString("Enter name");
+        DateTime dateOfBirth = ConsoleInput.GetDateTimeFromDateOnly("Enter date of birth [YYYY-MM-DD]");
 
-        // string lastFourNumbers = ConsoleInput.GetPersonNumberLastFourNumbers();
-        // PersonNumber personNumber = new PersonNumber(dateOfBirth, lastFourNumbers);
+        string lastFourNumbers = ConsoleInput.GetPersonNumberLastFourNumbers();
+        PersonNumber personNumber = new PersonNumber(dateOfBirth, lastFourNumbers);
 
-        // int cityId = ConsoleInput.GetInt("Enter the city id");
-        // string telephone = ConsoleInput.GetString("Enter phone number");
-        // string email = ConsoleInput.GetString("Enter email");
+        int cityId = ConsoleInput.GetInt("Enter the city id");
+        string telephone = ConsoleInput.GetString("Enter phone number");
+        string email = ConsoleInput.GetString("Enter email");
 
-        // JobSeeker j = new JobSeeker(name, dateOfBirth, personNumber.ToString(), cityId, telephone, email);
-
-        Random random = new();
-        JobSeeker j = new JobSeeker("Andrew Browne", new DateTime(1975, 1, 1), $"19751211{random.Next(1111, 9999)}", 72, "076876867", "me@email.com");
+        JobSeeker j = new JobSeeker(name, dateOfBirth, personNumber.ToString(), cityId, telephone, email);
 
         return j;
     }
